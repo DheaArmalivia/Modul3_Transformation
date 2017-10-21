a459e91fd96ec41082a868e2be5c320d1fcb817d
Dhea Armalivia <daffyderr@gmail.com>
2017-10-21 21:31:34 +0700

    Initial Commit

---
+++ Modul3/Source1.cpp
@@ -0,0 +1,44 @@
+#include <Windows.h>
+#include <GL\freeglut.h>
+#include <iostream>
+
+void kotak()
+{
+	//[a]
+	glTranslatef(200, -100, 0);
+	glBegin(GL_QUADS);
+	//[b]
+	glScalef(1, 0.25, 0);
+	glColor3f(1, 1, 1);
+	glVertex2f(100, 100);
+	glVertex2f(100, -100);
+	//[c]
+	glVertex2f(-100, -100);
+	glVertex2f(-100, 100);
+	//[d]
+	//glTranslatef(200, -100, 0);
+	glEnd();
+	//[e]
+}
+
+void render()
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glPushMatrix();
+	kotak();
+	glPopMatrix();
+	glutSwapBuffers();
+}
+
+int main(int argc, char* argv[])
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
+	glutInitWindowSize(800, 600);
+	glutCreateWindow("XIIRPL4-06-Dhea Armalivia Airizah");
+	gluOrtho2D(-400, 400, -300, 300);
+	glClearColor(0, 0, 0, 0);
+	glutDisplayFunc(render);
+	glutMainLoop();
+	return 0;
+}
